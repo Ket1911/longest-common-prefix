d509482ae0882cce877baba9fb623f3a078d4e68
Kateryna Vinskovska <vinskovskakate@gmail.com>
2023-09-27 17:50:09 +0300

    Update Solution.java

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,8 +1,27 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Solution {
     public String longestCommonPrefix(String[] strs) {
-        //write your code here
-        return "";
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+
+        Arrays.sort(strs);
+        String firstStr = strs[0];
+        String lastStr = strs[strs.length - 1];
+        int minLength = Math.min(firstStr.length(), lastStr.length());
+
+        StringBuilder commonPrefix = new StringBuilder();
+        for (int i = 0; i < minLength; i++) {
+            if (firstStr.charAt(i) == lastStr.charAt(i)) {
+                commonPrefix.append(firstStr.charAt(i));
+            } else {
+                break;
+            }
+        }
+
+        return commonPrefix.toString();
     }
 }
